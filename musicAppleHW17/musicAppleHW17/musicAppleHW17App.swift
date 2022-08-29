715b42a9eca8006740d59a48c654d70b229c83a5
Andrik <masmay.pl@gmail.com>
2022-08-29 19:07:50 +0200

    initial commit

---
+++ musicAppleHW17/musicAppleHW17/musicAppleHW17App.swift
@@ -0,0 +1,17 @@
+//
+//  musicAppleHW17App.swift
+//  musicAppleHW17
+//
+//  Created by Andrei Maskal on 29/08/2022.
+//
+
+import SwiftUI
+
+@main
+struct musicAppleHW17App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
